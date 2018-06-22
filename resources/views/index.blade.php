a6fa11dbafc0afd49e009e4414f3a36b6f28bab8
Alan25 <christian922017@gmail.com>
2018-06-22 13:32:04 -0500

    solo mostrando el login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,6 +51,9 @@
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Acceso Clientes</a>
+            </li>
           </ul>
         </div>
       </div>
